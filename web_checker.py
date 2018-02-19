c5f4b0c15c810ccdc5117f14457dc2df89453193
theNoob67 <mohdanny51@gmail.com>
2018-02-19 14:28:52 +0700

    versi 1.0

---
+++ web_checker.py
@@ -0,0 +1,17 @@
+def search():
+     web=raw_input("insert web site(without www.,ex:google.co.id,youtube.com):")
+     req=urllib2.urlopen("http://www."+web,timeout=5).getcode==200
+     print "Web exist"
+   
+a=True
+try:
+    
+ search()        
+
+     
+except urllib2.URLError as e:
+    print "Web not found"
+    search()
+except socket.timeout as e:
+    print "request timed out"
+    search()
