4227370bdf43d63901e234597a5e05afab0ee84d
theNoob67 <36617771+theNoob67@users.noreply.github.com>
2018-02-20 10:24:57 +0700

    Update web_checker.py

--- web_checker.py
+++ web_checker.py
@@ -1,3 +1,5 @@
+iport urllib2
+import requests
 def search():
      web=raw_input("insert web site(without www.,ex:google.co.id,youtube.com):")
      req=urllib2.urlopen("http://www."+web,timeout=5).getcode==200
